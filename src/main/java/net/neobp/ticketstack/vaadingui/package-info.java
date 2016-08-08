3faacd879c541fdfc19dc4efe0189335a3400eaf
Frank Endriss <fendriss@fum.de>
2016-08-08 16:56:48 +0200

    Added a Vaadin UI-class: VaadinUI.java

---
+++ src/main/java/net/neobp/ticketstack/vaadingui/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Frank Endriss (Frank.Endriss@vinci-energies.net)
+ * A Vaadin based GUI, simplest implementation (for testing).
+ *
+ */
+package net.neobp.ticketstack.vaadingui;
